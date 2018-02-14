800de850aa92cbbc6d35c295118820ebb651d343
Ehren Murdick <ehren.murdick@gmail.com>
2018-02-14 05:33:06 +0000

    it works!!!

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for chbs
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Lib
+import Data.List
+import System.Environment
+import System.IO
+import System.Random
+import Data.Char
+
+sanitize :: String -> String
+sanitize str = filter charsOnly $ map toLower str
+  where
+    charsOnly a = elem a ['a'..'z']
+
+getWord :: [String] -> Int -> String
+getWord words idx = sanitize (head (drop idx words)) ++ " "
+
+main :: IO ()
+main = do
+  contents <- readFile "/usr/share/dict/words"
+  let words = lines contents
+  gen <- getStdGen
+  let idxs = take 4 (randomRs (0, length words) gen)
+  let out = map (getWord words) idxs
+  mapM putStr out
+  putStrLn ""
